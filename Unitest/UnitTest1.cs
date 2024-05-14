12e7f17abbd595b59277d08eab54fb9a5a3f8d25
vimal <vimal@gmail.com>
2024-05-14 10:33:40 +0000

    MSBuild files Updated

---
+++ Unitest/UnitTest1.cs
@@ -0,0 +1,20 @@
+namespace Unitest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const string Expected = "Hello World!DevOps";
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                HelloWorld.Program.Main();
+
+                var result = sw.ToString().Trim();
+                Assert.AreEqual(Expected, result);
+            }
+        }
+    }
+}
